e9171221e1d78b98b1efcdd40c3f2a8264c4915b
isaacdanielvergara <isaacdanielvergaraflorez@gmail.com>
2023-11-04 14:54:11 -0500

    Se crea la interfaz grafica

--- src/proyecto2/pila.java
+++ src/proyecto2/pila.java
@@ -4,10 +4,54 @@
  */
 package proyecto2;
 
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author danie
  */
 public class pila {
+    
+    private Stack<producto> pila;
+    public pila(){
+        this.pila = new Stack();
+    }
+    
+    public void pushproduc(producto p){
+        if(existeid(p.id) != null){
+            JOptionPane.showMessageDialog(null,"El ID del producto ya se encuentra registrado");
+        }else if(existenom(p.nom) != null){
+            JOptionPane.showMessageDialog(null," El nombre del producto ya se encuentra registrado");
+        }else{
+            pila.push(p);
+            JOptionPane.showMessageDialog(null,"Producto registrado exitosamente!");
+        }
+    }
+    
+    public producto existeid (String id){
+        producto ide = new producto();
+        int i = 0;
+        while (i<pila.size()){
+            ide = (producto) pila.get(i);
+            if(ide.id.equals(id))
+                return ide;
+            i++;
+        }
+        ide = null;
+        return ide;
+    }
+    public producto existenom (String nom){
+        producto nome = new producto();
+        int i = 0;
+        while (i<pila.size()){
+            nome = (producto) pila.get(i);
+            if(nome.id.equals(nom))
+                return nome;
+            i++;
+        }
+        nome = null;
+        return nome;
+    }
     
 }
--- src/proyecto2/producto.java
+++ src/proyecto2/producto.java
@@ -6,8 +6,72 @@ package proyecto2;
 
 /**
  *
- * @author danie
+ * @author isaac
  */
 public class producto {
+    
+    String id;
+    String nom;
+    String fechal;
+    String fechav;
+    float preciou;
+    
+    public producto(){
+        id = "";
+        nom = "";
+        fechal = "";
+        fechav = "";
+        preciou = -1;
+    }
+    
+    public producto(String id, String nom, String fechal, String fechav, float preciou){
+        this.id = id;
+        this.nom = nom;
+        this.fechal = fechal;
+        this.fechav = fechav;
+        this.preciou = preciou;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getFechal() {
+        return fechal;
+    }
+
+    public void setFechal(String fechal) {
+        this.fechal = fechal;
+    }
+
+    public String getFechav() {
+        return fechav;
+    }
+
+    public void setFechav(String fechav) {
+        this.fechav = fechav;
+    }
+
+    public float getPreciou() {
+        return preciou;
+    }
+
+    public void setPreciou(float preciou) {
+        this.preciou = preciou;
+    }
+    
+    
     
 }
